d5d2f0e25be8c62fa626aba1b4788424240163d9
Thomas Leskin <tleskin@gmail.com>
2014-12-11 07:22:59 -0700

    Exercise 3: Numbers and Math

---
+++ ex3.rb
@@ -0,0 +1,29 @@
+#Outputs a message
+puts "I will now count my chickens:"
+#Outputs number of hens after calculating it
+puts "Hens #{25.0 + 30.0/6}"
+#Outputs number of roosters after calculating it
+puts "Roosters #{100.0 - 25 * 3.0 % 4}"
+#Outputs a message
+puts "Now I will count the eggs:"
+#Outputs the answer afte calculating
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1 / 4 + 6
+#Outputs a message
+puts "Is it true that 3 + 2 < 5 - 7?"
+#Calculates the sume and difference and
+#compares them
+puts 3.0 + 2 < 5.0 - 7
+#Calculates the values and outputs them with a message
+puts "What is 3 + 2? #{3.0 + 2}"
+puts "What is 5 - 7? #{5.0 - 7}"
+#Outputs a message
+puts "Oh, that's why it's false."
+#Outputs a message
+puts "How about some more."
+#Compares 5 and -2 with the greater than sign
+puts "Is it greater? #{5.0 > -2.0}"
+#Compares 5 and -2 with the greater than or equal
+# to sign
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+#Compares 5 and -2 with the less than sign
+puts "It is less or equal? #{5.0 <= -2.0}"
